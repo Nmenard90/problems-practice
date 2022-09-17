8ab84295ca1202d7b008b9e0ea43a51b516d61c8
Nick Menard <111668934+Nmenard90@users.noreply.github.com>
2022-09-16 22:54:46 -0500

    for/while loops
    Hacker rank problem for/while loop

---
+++ loop example.py
@@ -0,0 +1,12 @@
+#i<n<20
+#print i squared for each corresponding line in the range
+#range = 5
+
+
+def square():
+   i = 0
+   n = 20
+   for i in range(0,5):
+    if i < n:
+     print (i ** 2) 
+square()
